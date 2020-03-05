164dba7e2b6792a61fada43f6b1ec668f9e85c3f
unknown <ylasheen5@gmail.com>
2020-03-05 13:21:46 +0200

    Fix the bottom section of P2

--- lib/screens/projects_screen/projects_screen.dart
+++ lib/screens/projects_screen/projects_screen.dart
@@ -93,111 +93,144 @@ class ProjectsScreen extends StatelessWidget {
           SizedBox(
               height: MediaQuery.of(context).size.height * (4 / 3),
               child: Expanded(
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Color(0xFFc34372),
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.elliptical(500, 750),
-                          topRight: Radius.elliptical(500, 750))),
-                  child: FractionallySizedBox(
-                    widthFactor: 2 / 3,
-                    child: Column(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 6,
-                          child: Align(
-                            alignment: Alignment.topCenter,
-                            child: Image.asset(
-                                'assets/images/P2-BottonSection-Background.png'),
-                          ),
-                        ),
-                        Spacer(),
-                        Expanded(
-                          flex: 6,
-                          child: Column(
-                            children: <Widget>[
-                              Expanded(
-                                flex: 2,
-                                child: FittedBox(
-                                  child: Text("Have an intersting project?"),
-                                ),
-                              ),
-                              Expanded(
-                                child: FittedBox(
-                                  child: Text(
-                                    "No matter what it is you're after, contact me today to discuss further",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.normal,
-                                        color: Colors.white70),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Image.asset('assets/images/rect1208-0-8.png'),
+                    ),
+                    /*
+                    Padding(
+                      padding: const EdgeInsets.only(top: 150),
+                      child: Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Color(0xFFc34372),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(500),
+                                topRight: Radius.circular(500))),
+                      ),
+                    ),
+                    */
+                    Center(
+                      child: FractionallySizedBox(
+                        widthFactor: 2 / 3,
+                        child: Column(
+                          children: <Widget>[
+                            Spacer(flex: 2,),
+                            Expanded(
+                              flex: 6,
+                              child: Image.asset(
+                                  'assets/images/P2-BottonSection-Background.png'),
+                            ),
+                            Spacer(),
+                            Expanded(
+                              flex: 6,
+                              child: Column(
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 2,
+                                    child: FittedBox(
+                                      child:
+                                          Text("Have an intersting project?"),
+                                    ),
                                   ),
-                                ),
-                              ),
-                              Spacer(),
-                              Expanded(
-                                child: Center(
-                                  child: FractionallySizedBox(
-                                    widthFactor: 1 / 3,
-                                    child: Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          flex: 2,
-                                          child: TextField(
-                                            style:
-                                                TextStyle(color: Colors.black87),
-                                            decoration: new InputDecoration(
-                                                border: new OutlineInputBorder(
-                                                  borderSide: BorderSide.none,
-                                                  borderRadius:
-                                                      const BorderRadius.only(
-                                                    topLeft:
-                                                        const Radius.circular(
-                                                            50.0),
-                                                    bottomLeft:
-                                                        const Radius.circular(
-                                                            50.0),
-                                                  ),
-                                                ),
-                                                filled: true,
-                                                hintStyle: new TextStyle(
-                                                    color: Colors.grey[800]),
-                                                hintText: "Your Email",
-                                                fillColor: Colors.white),
-                                          ),
-                                        ),
-                                        Expanded(
-                                          child: SizedBox.expand(
-                                            child: FlatButton(
-                                              onPressed: (){},
-                                              color: Colors.cyan,
-                                              child: Text("GO",style: TextStyle(color: Colors.black),),
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    const BorderRadius.only(
-                                                  bottomRight:
-                                                      const Radius.circular(50.0),
-                                                  topRight:
-                                                      const Radius.circular(50.0),
+                                  Expanded(
+                                    child: FittedBox(
+                                      child: Text(
+                                        "No matter what it is you're after, contact me today to discuss further",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.normal,
+                                            color: Colors.white70),
+                                      ),
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Center(
+                                      child: FractionallySizedBox(
+                                        widthFactor: 5 / 12,
+                                        child: Row(
+                                          children: <Widget>[
+                                            Expanded(
+                                              flex: 3,
+                                              child: SizedBox.expand(
+                                                child: TextField(
+                                                  style: TextStyle(
+                                                      color: Colors.black87),
+                                                  decoration:
+                                                      new InputDecoration(
+                                                          border:
+                                                              new OutlineInputBorder(
+                                                            borderSide:
+                                                                BorderSide.none,
+                                                            borderRadius:
+                                                                const BorderRadius
+                                                                    .only(
+                                                              topLeft: const Radius
+                                                                      .circular(
+                                                                  50.0),
+                                                              bottomLeft:
+                                                                  const Radius
+                                                                          .circular(
+                                                                      50.0),
+                                                            ),
+                                                          ),
+                                                          filled: true,
+                                                          hintStyle:
+                                                              new TextStyle(
+                                                                  color: Colors
+                                                                          .grey[
+                                                                      800]),
+                                                          hintText:
+                                                              "Your Email",
+                                                          fillColor:
+                                                              Colors.white),
                                                 ),
                                               ),
                                             ),
-                                          ),
-                                        )
-                                      ],
+                                            Expanded(
+                                              child: SizedBox.expand(
+                                                child: FlatButton(
+                                                  onPressed: () {},
+                                                  color: Colors.cyan,
+                                                  child: Text(
+                                                    "GO",
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        const BorderRadius.only(
+                                                      bottomRight:
+                                                          const Radius.circular(
+                                                              50.0),
+                                                      topRight:
+                                                          const Radius.circular(
+                                                              50.0),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
                                     ),
                                   ),
-                                ),
+                                  Spacer()
+                                ],
                               ),
-                              Spacer(
-                                flex: 3,
-                              )
-                            ],
-                          ),
-                        )
-                      ],
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ))
         ],
