679ebb6e9350899a3588ff432421dfb76d9ce040
unknown <ylasheen5@gmail.com>
2020-03-06 15:38:05 +0200

    Replace SIngleChildScrollView by ListView.builder to improve performance

--- lib/screens/projects_screen/projects_screen.dart
+++ lib/screens/projects_screen/projects_screen.dart
@@ -7,10 +7,11 @@ class ProjectsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
+    return ListView.builder(
+      itemCount: projects.length + 2,
+      itemBuilder: (BuildContext context, int index) {
+        if (index == 0) {
+          return SizedBox(
             height: MediaQuery.of(context).size.height / 2,
             child: Column(
               children: <Widget>[
@@ -59,180 +60,173 @@ class ProjectsScreen extends StatelessWidget {
                 Spacer(flex: 2)
               ],
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height *
-                    (projects.length * (2 / 3)) +
-                (projects.length * 100),
-            child: Column(
+          );
+        } else if (index == projects.length + 1) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * (4 / 3),
+            child: Stack(
               children: <Widget>[
-                for (int i = 0; i < projects.length; i++)
-                  if (i.isEven) ...[
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.only(bottom: 100),
-                        child: ProjectCard(
-                          project: projects[i],
-                          isInversed: true,
-                        ),
-                      ),
-                    )
-                  ] else
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.only(bottom: 100),
-                        child: ProjectCard(
-                          project: projects[i],
-                          isInversed: false,
-                        ),
-                      ),
-                    )
-              ],
-            ),
-          ),
-          SizedBox(
-              height: MediaQuery.of(context).size.height * (4 / 3),
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Image.asset('assets/images/rect1208-0-8.png'),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Image.asset('assets/images/rect1208-0-8.png'),
+                ),
+                /*
+                Padding(
+                  padding: const EdgeInsets.only(top: 150),
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFc34372),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(500),
+                            topRight: Radius.circular(500))),
                   ),
-                  /*
-                  Padding(
-                    padding: const EdgeInsets.only(top: 150),
-                    child: Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                          color: Color(0xFFc34372),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(500),
-                              topRight: Radius.circular(500))),
-                    ),
-                  ),
-                  */
-                  Center(
-                    child: FractionallySizedBox(
-                      widthFactor: 2 / 3,
-                      child: Column(
-                        children: <Widget>[
-                          Spacer(flex: 2,),
-                          Expanded(
-                            flex: 6,
-                            child: Image.asset(
-                                'assets/images/P2-BottonSection-Background.png'),
-                          ),
-                          Spacer(),
-                          Expanded(
-                            flex: 6,
-                            child: Column(
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 2,
-                                  child: FittedBox(
-                                    child:
-                                        Text("Have an intersting project?"),
+                ),
+                */
+                Center(
+                  child: FractionallySizedBox(
+                    widthFactor: 2 / 3,
+                    child: Column(
+                      children: <Widget>[
+                        Spacer(
+                          flex: 2,
+                        ),
+                        Expanded(
+                          flex: 6,
+                          child: Image.asset(
+                              'assets/images/P2-BottonSection-Background.png'),
+                        ),
+                        Spacer(),
+                        Expanded(
+                          flex: 6,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: FittedBox(
+                                  child: Text("Have an intersting project?"),
+                                ),
+                              ),
+                              Expanded(
+                                child: FittedBox(
+                                  child: Text(
+                                    "No matter what it is you're after, contact me today to discuss further",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.white70),
                                   ),
                                 ),
-                                Expanded(
-                                  child: FittedBox(
-                                    child: Text(
-                                      "No matter what it is you're after, contact me today to discuss further",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.normal,
-                                          color: Colors.white70),
-                                    ),
-                                  ),
-                                ),
-                                Spacer(),
-                                Expanded(
-                                  flex: 1,
-                                  child: Center(
-                                    child: FractionallySizedBox(
-                                      widthFactor: 5 / 12,
-                                      child: Row(
-                                        children: <Widget>[
-                                          Expanded(
-                                            flex: 3,
-                                            child: SizedBox.expand(
-                                              child: TextField(
+                              ),
+                              Spacer(),
+                              Expanded(
+                                flex: 1,
+                                child: Center(
+                                  child: FractionallySizedBox(
+                                    widthFactor: 5 / 12,
+                                    child: Row(
+                                      children: <Widget>[
+                                        Expanded(
+                                          flex: 3,
+                                          child: SizedBox.expand(
+                                            child: TextField(
+                                              style: TextStyle(
+                                                  color: Colors.black87),
+                                              decoration: new InputDecoration(
+                                                  border:
+                                                      new OutlineInputBorder(
+                                                    borderSide: BorderSide.none,
+                                                    borderRadius:
+                                                        const BorderRadius.only(
+                                                      topLeft:
+                                                          const Radius.circular(
+                                                              50.0),
+                                                      bottomLeft:
+                                                          const Radius.circular(
+                                                              50.0),
+                                                    ),
+                                                  ),
+                                                  filled: true,
+                                                  hintStyle: new TextStyle(
+                                                      color: Colors.grey[800]),
+                                                  hintText: "Your Email",
+                                                  fillColor: Colors.white),
+                                            ),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: SizedBox.expand(
+                                            child: FlatButton(
+                                              onPressed: () {},
+                                              color: Colors.cyan,
+                                              child: Text(
+                                                "GO",
                                                 style: TextStyle(
-                                                    color: Colors.black87),
-                                                decoration:
-                                                    new InputDecoration(
-                                                        border:
-                                                            new OutlineInputBorder(
-                                                          borderSide:
-                                                              BorderSide.none,
-                                                          borderRadius:
-                                                              const BorderRadius
-                                                                  .only(
-                                                            topLeft: const Radius
-                                                                    .circular(
-                                                                50.0),
-                                                            bottomLeft:
-                                                                const Radius
-                                                                        .circular(
-                                                                    50.0),
-                                                          ),
-                                                        ),
-                                                        filled: true,
-                                                        hintStyle:
-                                                            new TextStyle(
-                                                                color: Colors
-                                                                        .grey[
-                                                                    800]),
-                                                        hintText:
-                                                            "Your Email",
-                                                        fillColor:
-                                                            Colors.white),
+                                                    color: Colors.white),
+                                              ),
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    const BorderRadius.only(
+                                                  bottomRight:
+                                                      const Radius.circular(
+                                                          50.0),
+                                                  topRight:
+                                                      const Radius.circular(
+                                                          50.0),
+                                                ),
                                               ),
                                             ),
                                           ),
-                                          Expanded(
-                                            child: SizedBox.expand(
-                                              child: FlatButton(
-                                                onPressed: () {},
-                                                color: Colors.cyan,
-                                                child: Text(
-                                                  "GO",
-                                                  style: TextStyle(
-                                                      color: Colors.white),
-                                                ),
-                                                shape: RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      const BorderRadius.only(
-                                                    bottomRight:
-                                                        const Radius.circular(
-                                                            50.0),
-                                                    topRight:
-                                                        const Radius.circular(
-                                                            50.0),
-                                                  ),
-                                                ),
-                                              ),
-                                            ),
-                                          )
-                                        ],
-                                      ),
+                                        )
+                                      ],
                                     ),
                                   ),
                                 ),
-                                Spacer()
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                              ),
+                              Spacer()
+                            ],
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                ],
-              ))
-        ],
-      ),
+                ),
+              ],
+            ),
+          );
+        }
+        index = index - 1;
+        return SizedBox(
+          height: MediaQuery.of(context).size.height * (1 / 3) +
+              (projects.length * 100),
+          child: Column(
+            children: <Widget>[
+              if (index.isEven) ...[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 100),
+                    child: ProjectCard(
+                      project: projects[index],
+                      isInversed: true,
+                    ),
+                  ),
+                )
+              ] else
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 100),
+                    child: ProjectCard(
+                      project: projects[index],
+                      isInversed: false,
+                    ),
+                  ),
+                )
+            ],
+          ),
+        );
+      },
     );
   }
 }
