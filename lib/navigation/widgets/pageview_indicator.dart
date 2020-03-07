18a39722aec36bff77b2a038918ba983c01c4fb8
unknown <ylasheen5@gmail.com>
2020-03-07 15:50:35 +0200

    Fix responsiveness  problems (2) (SideBar - landsacpe - Small screens)

--- lib/navigation/widgets/pageview_indicator.dart
+++ lib/navigation/widgets/pageview_indicator.dart
@@ -8,24 +8,26 @@ class PageViewIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<String> list = ['ABOUT', 'PROJECTS', 'CONTACT'];
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 20),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Spacer(
-            flex: 4,
-          ),
-          GestureDetector(onTap:()=>callback(2),child: circleBar(activePage == 2, list[2])),
-          Spacer(),
-          GestureDetector(onTap:()=>callback(1),child: circleBar(activePage == 1, list[1])),
-          Spacer(),
-          GestureDetector(onTap:()=>callback(0),child: circleBar(activePage == 0, list[0])),
-          Spacer(
-            flex: 4,
-          ),
-        ],
+    return Center(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.width * 0.028,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Spacer(
+              flex: 4,
+            ),
+            GestureDetector(onTap:()=>callback(2),child: circleBar(activePage == 2, list[2])),
+            Spacer(),
+            GestureDetector(onTap:()=>callback(1),child: circleBar(activePage == 1, list[1])),
+            Spacer(),
+            GestureDetector(onTap:()=>callback(0),child: circleBar(activePage == 0, list[0])),
+            Spacer(
+              flex: 4,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -34,19 +36,25 @@ class PageViewIndicator extends StatelessWidget {
 Widget circleBar(bool isActive, String content) {
   return Column(
     children: <Widget>[
-      Padding(
-          padding: EdgeInsets.symmetric(vertical: 5),
-          child: Text(
-            content,
-            style: TextStyle(color: isActive ? Colors.white : Colors.white70),
-          )),
-      AnimatedContainer(
-        duration: Duration(milliseconds: 150),
-        margin: EdgeInsets.symmetric(horizontal: 8),
-        height: 4,
-        width: 70,
-        color: isActive ? Color(0xFFc34372) : Colors.transparent,
+      Spacer(),
+      Expanded(
+        flex: 6,
+        child: Text(
+          content,
+          style: TextStyle(color: isActive ? Colors.white : Colors.white70),
+        ),
       ),
+      Spacer(flex: 2,),
+      Expanded(
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 150),
+          margin: EdgeInsets.symmetric(horizontal: 8),
+          height: 4,
+          width: 70,
+          color: isActive ? Color(0xFFc34372) : Colors.transparent,
+        ),
+      ),
+      Spacer()
     ],
   );
 }
