b863902dfb0d05719e41c48ed568742b3de3d676
unknown <ylasheen5@gmail.com>
2020-03-08 17:24:47 +0200

    Add functionality to the FlatButton in P1

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:auto_size_text/auto_size_text.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({Key key}) : super(key: key);
@@ -12,9 +12,9 @@ class AboutScreen extends StatelessWidget {
         MediaQuery.of(context).orientation == Orientation.landscape;
     return Row(
       children: <Widget>[
-        isLandscape?Container(): Spacer(),
+        isLandscape ? Container() : Spacer(),
         Expanded(
-          flex: isLandscape? 5:15,
+          flex: isLandscape ? 5 : 15,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -23,14 +23,11 @@ class AboutScreen extends StatelessWidget {
                 flex: 3,
               ),
               Expanded(
-                flex: isLandscape? 5: 3,
+                flex: isLandscape ? 5 : 3,
                 child: AutoSizeText(
                   "Hello, I'm\nYoussef Lasheen",
                   textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 150
-                  ),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 150),
                   maxLines: 2,
                 ),
               ),
@@ -38,7 +35,7 @@ class AboutScreen extends StatelessWidget {
                 flex: 6,
                 child: AutoSizeText(
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed rhoncus neque. Pellentesque venenatis felis at risus rutrum porttitor ac sed turpis. Praesent vel iaculis lacus. Mauris maximus quam congue neque tincidunt aliquam vel in sapien.',
-                  style: TextStyle(color: Colors.grey,fontSize: 50),
+                  style: TextStyle(color: Colors.grey, fontSize: 50),
                 ),
               ),
               Expanded(
@@ -49,7 +46,9 @@ class AboutScreen extends StatelessWidget {
                       flex: 2,
                       child: SizedBox.expand(
                         child: FlatButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            _launchURL();
+                          },
                           child: FittedBox(child: Text('Download CV')),
                           color: Color(0xFF323941),
                           shape: RoundedRectangleBorder(
@@ -58,21 +57,8 @@ class AboutScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Spacer(),
-                    Expanded(
-                      child: SizedBox.expand(
-                        child: FloatingActionButton(
-                          onPressed: () {},
-                          child: Icon(
-                            Icons.arrow_drop_down,
-                            color: Colors.white,
-                          ),
-                          backgroundColor: Color(0xFFc34372),
-                        ),
-                      ),
-                    ),
                     Spacer(
-                      flex: 2,
+                      flex: 4,
                     )
                   ],
                 ),
@@ -84,20 +70,32 @@ class AboutScreen extends StatelessWidget {
           ),
         ),
         Spacer(),
-        isLandscape? Expanded(
-          flex: 3,
-          child: Column(
-            children: <Widget>[
-              Spacer(),
-              Image.asset('assets/images/P1Logo.png'),
-              Spacer(
+        isLandscape
+            ? Expanded(
                 flex: 3,
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Image.asset('assets/images/P1Logo.png'),
+                    Spacer(
+                      flex: 3,
+                    )
+                  ],
+                ),
               )
-            ],
-          ),
-        ):Container(),
-        isLandscape? Spacer():Container(),
+            : Container(),
+        isLandscape ? Spacer() : Container(),
       ],
     );
+  }
+}
+
+_launchURL() async {
+  const url =
+      'https://firebasestorage.googleapis.com/v0/b/yousseflasheen-portfolio.appspot.com/o/Resume%20v4%20(1).pdf?alt=media&token=00434306-5ac2-408e-b7be-356692b6e05e';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
