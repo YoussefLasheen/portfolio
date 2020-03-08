4631de9cc03f082f9f8c45d47671b5bcce3fa790
unknown <ylasheen5@gmail.com>
2020-03-08 19:04:57 +0200

    Add Firebase to the project

---
+++ lib/services/api.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+
+
+class Api {
+  final Firestore _db = Firestore.instance;
+  final String path;
+  CollectionReference ref;
+
+  Api(this.path) {
+    ref = _db.collection(path);
+  }
+
+  Future<QuerySnapshot> getDataCollection() {
+    return ref.getDocuments() ;
+  }
+  
+  Future<DocumentSnapshot> getDocumentById(String id) {
+    return ref.document(id).get();
+  }
+}
