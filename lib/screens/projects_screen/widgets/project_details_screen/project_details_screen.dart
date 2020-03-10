b1ef25ad519c15e142daafbe74c2304d5e471066
unknown <ylasheen5@gmail.com>
2020-03-10 18:49:28 +0200

    Add markdown in the details page

--- lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
+++ lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:portfolio/screens/projects_screen/models/project.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProjectDetails extends StatelessWidget {
   const ProjectDetails({Key key, this.project}) : super(key: key);
@@ -8,6 +10,9 @@ class ProjectDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    String body = project.details['body'].replaceAll('<br/>', '\n');
     return Material(
       child: CustomScrollView(
         slivers: <Widget>[
@@ -27,10 +32,30 @@ class ProjectDetails extends StatelessWidget {
             ),
           ),
           SliverFillRemaining(
-              child: Text("data"),
+            hasScrollBody: true,
+            child: Center(
+              child: FractionallySizedBox(
+                widthFactor: isLandscape ? 3 / 4 : 1,
+                child: Markdown(
+                  data: body,
+                  onTapLink: (String url) => _launchURL(url),
+                  styleSheet: MarkdownStyleSheet(
+                    blockSpacing: 20,
+                  ),
+                ),
+              ),
             ),
+          ),
         ],
       ),
     );
+  }
+}
+
+_launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
