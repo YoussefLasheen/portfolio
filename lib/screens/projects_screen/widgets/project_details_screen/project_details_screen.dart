3826a187da2b6e743a62ab60671e94bdded55df4
Youssef Lasheen <youssef@lasheen.dev>
2021-11-14 10:54:44 +0200

    Unify the logic of projects details and limit the data passing through with the new metadata variable

--- lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
+++ lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
@@ -5,33 +5,28 @@ import 'package:portfolio/screens/projects_screen/models/project.dart';
 import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/widgets/project_details_screen_top_landscape.dart';
 import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/widgets/project_details_screen_top_portrait.dart';
 
-import 'widgets/project_details.dart';
 
 class ProjectDetailsScreen extends StatelessWidget {
-  const ProjectDetailsScreen({this.isInversed, this.project});
+  const ProjectDetailsScreen({this.isInversed, this.projectMetadata});
 
-  final Project project;
+  final ProjectMetadata projectMetadata;
   final bool isInversed;
 
   @override
   Widget build(BuildContext context) {
     bool isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
-    String body = project.details['body'].replaceAll('<br/>', '\n');
-    return isLandscape
-        ? ProjectDetailsScreenTopLandscape(
+    if (isLandscape) {
+      return ProjectDetailsScreenTopLandscape(
             isInversed: isInversed,
-            project: project,
-            content: ProjectDetails(
-              body: body,
-            ),
-          )
-        : ProjectDetailsScreenTopPotrait(
-            project: project,
-            content: ProjectDetails(
-              body: body,
-            ),
+            projectMetadata: projectMetadata,
+
           );
+    } else {
+      return ProjectDetailsScreenTopPotrait(
+            projectMetadata: projectMetadata,
+          );
+    }
   }
 }
 
--- lib/screens/projects_screen/widgets/project_details_screen/widgets/project_details.dart
+++ lib/screens/projects_screen/widgets/project_details_screen/widgets/project_details.dart
@@ -1,27 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:portfolio/screens/projects_screen/models/project.dart';
+import 'package:portfolio/services/api.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ProjectDetails extends StatelessWidget {
-  const ProjectDetails({this.body});
-  final String body;
+  const ProjectDetails({this.projectMetadata});
+  final ProjectMetadata projectMetadata;
   @override
   Widget build(BuildContext context) {
     bool isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: FractionallySizedBox(
-        widthFactor: isLandscape ? 3 / 4 : 1,
-        child: MarkdownBody(
-          data: body,
-          onTapLink: (String url) => _launchURL(url),
-          styleSheet: MarkdownStyleSheet(
-            blockSpacing: 20,
+    return FutureBuilder<DocumentSnapshot>(
+      future: Future.delayed(
+            const Duration(
+              milliseconds: 350,
+            ), () {
+          return Api('Data/projects_screen/projects').getDocumentById(projectMetadata.id);
+        }),
+      builder: (context, snapshot) {
+        ProjectDescription projectContent;
+          if (!snapshot.hasData) {
+            projectContent = ProjectDescription(text: 'asfd',projectMetadata: projectMetadata);
+          } else {
+            projectContent = ProjectDescription(text: snapshot.data['content'],projectMetadata: projectMetadata);
+          }
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FractionallySizedBox(
+            widthFactor: isLandscape ? 3 / 4 : 1,
+            child: Column(
+              children: [
+                Text(projectContent.projectMetadata.title),
+                PlatformIndicator({'Android':true,'Ios':true,'Web':true,'Windows':true,'Mac':true,'Linux':true,}),
+                LicenseIndicator(),
+                MarkdownBody(
+                  data: projectMetadata.title,
+                  onTapLink: (String url) => _launchURL(url),
+                  styleSheet: MarkdownStyleSheet(
+                    blockSpacing: 20,
+                  ),
+                  selectable: true,
+                ),
+              ],
+            ),
           ),
-          selectable: true,
-        ),
-      ),
+        );
+      }
     );
   }
 }
@@ -30,5 +57,37 @@ _launchURL(String url) async {
     await launch(url);
   } else {
     throw 'Could not launch $url';
+  }
+}
+
+class PlatformIndicator extends StatelessWidget {
+  const PlatformIndicator(this.platforms);
+  final Map platforms;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text("Available for"),
+      platforms['Android']==true?Icon(Icons.android):Container(),
+      platforms['Ios']==true?Icon(Icons.phone_iphone_rounded):Container(),
+      platforms['Web']==true?Icon(Icons.language):Container(),
+      platforms['Windows']==true?Icon(Icons.window):Container(),
+      platforms['Mac']==true?Icon(Icons.laptop_mac):Container(),
+      platforms['Linux']==true?Icon(Icons.precision_manufacturing_outlined):Container()
+    ],);
+  }
+}
+
+class LicenseIndicator extends StatelessWidget {
+  const LicenseIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text("License is:"),
+        Icon(Icons.military_tech)
+      ],
+    );
   }
 }
