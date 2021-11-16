986aea4b1a241018bf264a62c6fe69c05422ac91
Youssef Lasheen <youssef@lasheen.dev>
2021-11-16 21:20:41 +0200

    Connect the router to the app with materialApp.router .

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/navigation/root.dart';
+import 'package:portfolio/routes/router.gr.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: "Youssef Lasheen's Portfolio",
       theme: ThemeData.dark(),
-      home: Root(),
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
