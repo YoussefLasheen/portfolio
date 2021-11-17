f0767a3d1c5be9bae0c85426fcde5a76ffc4653d
Youssef Lasheen <youssef@lasheen.dev>
2021-11-18 01:17:04 +0200

    Add a simple filter by a tag to the projects screen Make AnimatedFilteredList (To be animated) to separate the state

--- lib/screens/projects_screen/projects_screen.dart
+++ lib/screens/projects_screen/projects_screen.dart
@@ -20,6 +20,7 @@ class ProjectsScreen extends StatelessWidget {
       }),
       builder: (context, snapshot) {
         List projectsIndex;
+        List allTags;
         if (!snapshot.hasData) {
           projectsIndex = [
             {
@@ -30,30 +31,112 @@ class ProjectsScreen extends StatelessWidget {
               'tags': []
             }
           ];
+          allTags = [''];
         } else {
           projectsIndex = snapshot.data['projectsIndex'];
+          allTags = snapshot.data['allTags'];
         }
 
-        return ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: projectsIndex.length + 1,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
-              return TopSection();
-            }
-            index = index - 1;
-            return ProjectCard(
-              projectMetadata: ProjectMetadata(
-                title: projectsIndex[index]['title'],
-                shortDescription: projectsIndex[index]['shortDescription'],
-                backgroundImageSource: projectsIndex[index]
-                    ['backgroundImageSource'],
-                tags: projectsIndex[index]['tags'],
-                id: projectsIndex[index]['id'],
+        return AnimatedFilteredList(
+            projectsIndex: projectsIndex, allTags: allTags);
+      },
+    );
+  }
+}
+
+class AnimatedFilteredList extends StatefulWidget {
+  final List allTags;
+  const AnimatedFilteredList({
+    Key key,
+    @required this.projectsIndex,
+    this.allTags,
+  }) : super(key: key);
+
+  final List projectsIndex;
+
+  @override
+  State<AnimatedFilteredList> createState() => _AnimatedFilteredListState();
+}
+
+class _AnimatedFilteredListState extends State<AnimatedFilteredList> {
+  String filterTag = "All";
+  
+  @override
+  Widget build(BuildContext context) {
+    List filteredIndex = [];
+    if(filterTag=="All"){
+      filteredIndex = widget.projectsIndex;
+    }else{
+      for (int i = 0; i < widget.projectsIndex.length; i++) {
+      for (int h = 0; h < widget.projectsIndex[i]['tags'].length; h++) {
+        if (filterTag.contains(widget.projectsIndex[i]['tags'][h])) {
+          filteredIndex.add(widget.projectsIndex[i]);
+          break;
+        }
+      }
+    }
+    }
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: filteredIndex.length + 1,
+      itemBuilder: (BuildContext context, int index) {
+        if (index == 0) {
+          return Column(
+            children: [
+              TopSection(),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ChoiceChip(
+                            selectedColor: Color(0xFFc34372),
+                                label: Text("All",style: TextStyle(color: Colors.white),),
+                                selected: filterTag.contains("All"),
+                                onSelected: (s) {
+                                  filterTag = "All";
+                                  setState(() {});
+                                },
+                              ),
+                        ),
+                        for (var tag in widget.allTags)
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: ChoiceChip(
+                              selectedColor: Color(0xFFc34372),
+                              label: Text(tag,style: TextStyle(color: Colors.white),),
+                              selected: filterTag.contains(tag),
+                              onSelected: (s) {
+                                filterTag = tag;
+                                setState(() {});
+                              },
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                ),
               ),
-              isInversed: index.isEven,
-            );
-          },
+            ],
+          );
+        }
+        index = index - 1;
+        return ProjectCard(
+          //id: filteredIndex[index]['id'],
+          projectMetadata: ProjectMetadata(
+            title: filteredIndex[index]['title'],
+            shortDescription: filteredIndex[index]['shortDescription'],
+            backgroundImageSource: filteredIndex[index]
+                ['backgroundImageSource'],
+            tags: filteredIndex[index]['tags'],
+            id: filteredIndex[index]['id'],
+          ),
+          isInversed: index.isEven,
         );
       },
     );
