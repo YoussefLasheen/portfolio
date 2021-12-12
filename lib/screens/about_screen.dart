68ee9714b23b1ccdb28c824ebb12183591fa2ce7
Youssef Lasheen <youssef@lasheen.dev>
2021-12-12 23:01:09 +0200

    Fix screen sizing on small screens on the about_screen.

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -25,108 +25,75 @@ class AboutScreen extends StatelessWidget {
                     ),
               Expanded(
                 flex: isLandscape ? 20 : 60,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Spacer(
-                      flex: 6,
-                    ),
-                    Expanded(
-                      flex: isLandscape ? 10 : 6,
-                      child: AutoSizeText(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 60),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      AutoSizeText(
                         "Hello, I'm\nYoussef Lasheen",
                         textAlign: TextAlign.left,
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 150),
                         maxLines: 2,
                       ),
-                    ),
-                    Spacer(
-                      flex: 2,
-                    ),
-                    Expanded(
-                      flex: 10,
-                      child: AutoSizeText(
+                      SizedBox(height: 20,),
+                      AutoSizeText(
                         'A freelance Flutter developer based in Egypt.\nI make quality, commented, and reusable code.\nMy services include product analysis through to full product design - from wireframing and prototyping to interface design and Flutter development.',
                         style: TextStyle(
                           color: Colors.white70,
-                          fontSize: 30,
+                          fontSize: 25,
                           fontWeight: FontWeight.w100,
+                          height: 1.3
                         ),
+                        maxLines: 6,
                       ),
-                    ),
-                    !isLandscape
-                        ? Container()
-                        : Expanded(
-                            flex: 3,
-                            child: Row(
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 3,
-                                  child: SizedBox.expand(
-                                    child: OutlinedButton(
-                                      style: OutlinedButton.styleFrom(
-                                        backgroundColor: Color(0xFF323941),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              new BorderRadius.circular(1500.0),
-                                        ),
-                                      ),
-                                      onPressed: () {
-                                        _launchURL();
-                                      },
-                                      child: SizedBox.expand(
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: FittedBox(
-                                            child: Text(
-                                              'DOWNLOAD CV',
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.white),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
+                      SizedBox(height: 20,),
+                      !isLandscape
+                          ? Container()
+                          : Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                  backgroundColor: Color(0xFF323941),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(1500.0),
                                   ),
                                 ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Expanded(
-                                  flex: 1,
-                                  child: SizedBox.expand(
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Color(0xFFc34372),
-                                        shape: CircleBorder()
-                                      ),
-                                      onPressed: () {
-                                        context.tabsRouter.setActiveIndex(1);
-                                      },
-                                      child: SizedBox.expand(
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: FittedBox(
-                                              child: Icon(Icons
-                                                  .arrow_downward_rounded)),
-                                        ),
-                                      ),
-                                    ),
+                                onPressed: () {
+                                  _launchURL();
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(18.0),
+                                  child: Text(
+                                    'DOWNLOAD CV',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        fontSize: 20),
                                   ),
                                 ),
-                                Spacer(
-                                  flex: 8,
-                                )
-                              ],
-                            ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              FloatingActionButton(
+                                backgroundColor: Color(0xFFc34372),
+                                child: Icon(
+                                  Icons.arrow_downward_rounded,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  context.tabsRouter.setActiveIndex(1);
+                                },
+                                ),
+                            ],
                           ),
-                    Spacer(
-                      flex: 12,
-                    )
-                  ],
+                    ],
+                  ),
                 ),
               ),
               Spacer(
