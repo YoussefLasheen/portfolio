4c31f82219b1789329f1d6d159fcb612f3144d1f
Youssef Lasheen <youssef@lasheen.dev>
2021-12-17 17:35:58 +0200

    Add a constants file with the main gradient used in the app

---
+++ lib/assets/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Gradient mainGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [
+      0.05,
+      0.95,
+    ],
+    colors: [
+      Color.fromRGBO(22, 0, 64, 1),
+      Color.fromRGBO(121, 1, 140, 1),
+    ]);
+
