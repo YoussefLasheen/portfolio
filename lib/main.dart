e2b4b92320a0cb4c0682c332484bdba278a0be4d
Youssef Lasheen <youssef@lasheen.dev>
2021-12-19 23:35:15 +0200

    Add firebase for the android version

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/navigation/root.dart';
 import 'package:portfolio/routes/router.gr.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   final _appRouter = AppRouter();
