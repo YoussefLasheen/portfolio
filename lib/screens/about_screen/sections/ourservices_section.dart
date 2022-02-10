73f8fa1e3e2350291ea7ae20f740d7f9ce53d208
Youssef Lasheen <youssef@lasheen.dev>
2022-02-10 23:02:43 +0200

    Animate icons on hover with rive

--- lib/screens/about_screen/sections/ourservices_section.dart
+++ lib/screens/about_screen/sections/ourservices_section.dart
@@ -1,8 +1,10 @@
 import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:portfolio/assets/constants.dart';
 import 'package:portfolio/screens/shared_components/frostedglass_container.dart';
+import 'package:rive/rive.dart';
 
 class OurservicesSection extends StatelessWidget {
   @override
@@ -57,47 +59,29 @@ class OurservicesCard extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            _buildTile('Mobile Development', '4 projects', ()=>context.router.navigateNamed('projects?tag=Mobile'),
-                AssetImage('assets/icons/mobile-development.png')),
+            AnimatedTile(
+                'Mobile Development',
+                '4 projects',
+                () => context.router.navigateNamed('projects?tag=Mobile'),
+                'assets/animations/mobile-development.riv'),
             _buildSeparator(),
-            _buildTile('Web Development', '4 projects', ()=>context.router.navigateNamed('projects?tag=Web'),
-                AssetImage('assets/icons/web-development.png')),
+            AnimatedTile(
+                'Web Development',
+                '4 projects',
+                () => context.router.navigateNamed('projects?tag=Web'),
+                'assets/animations/web-development.riv'),
             _buildSeparator(),
-            _buildTile('UI/UX Design', '4 projects', ()=>context.router.navigateNamed('projects'),
-                AssetImage('assets/icons/ui-design.png')),
+            AnimatedTile(
+                'UI/UX Design',
+                '4 projects',
+                () => context.router.navigateNamed('projects'),
+                'assets/animations/ui-design.riv'),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildTile(
-      String text, String subText, Function link, ImageProvider image) {
-    return Row(
-      children: [
-        Image(
-          image: image,
-          height: 60,
-          width: 60,
-        ),
-        SizedBox(
-          width: 40,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(text),
-            Text(
-              subText,
-              style: TextStyle(fontSize: 12, color: Colors.white30),
-            )
-          ],
-        ),
-        Spacer(),
-        IconButton(onPressed: link as void Function()?, icon: Icon(Icons.navigate_next_rounded))
-      ],
-    );
-  }
 
   Widget _buildSeparator() {
     return Divider(
@@ -105,6 +89,78 @@ class OurservicesCard extends StatelessWidget {
       indent: 50,
       endIndent: 50,
       color: Colors.white12,
+    );
+  }
+}
+class AnimatedTile extends StatefulWidget {
+  final String text;
+  final String subText;
+  final Function link;
+  final String animationPath;
+
+    AnimatedTile( this.text, this.subText, this.link, this.animationPath,);
+  
+    @override
+    State<AnimatedTile> createState() => _AnimatedTileState();
+  }
+  
+class _AnimatedTileState extends State<AnimatedTile> {
+  SMIInput<bool>? _hoveringInput;
+  Artboard? _artboard;
+
+  @override
+  void initState() {
+    super.initState();
+    rootBundle.load(widget.animationPath).then(
+      (data) {
+        final file = RiveFile.import(data);
+        final artboard = file.mainArtboard;
+        var controller = StateMachineController.fromArtboard(
+          artboard,
+          'State Machine 1',
+        );
+        if (controller != null) {
+          artboard.addController(controller);
+          _hoveringInput = controller.findInput('hovering');
+        }
+        setState(() => _artboard = artboard);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.link as void Function()?,
+      onHover: (val)=>_hoveringInput?.value = val,
+      child: Row(
+        children: [
+          SizedBox.square(
+            dimension: 60,
+            child: _artboard == null
+                ? SizedBox()
+                : Rive(
+                    artboard: _artboard!,
+                    fit: BoxFit.fitHeight,
+                  ),
+          ),
+          SizedBox(
+            width: 40,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(widget.text),
+              Text(
+                widget.text,
+                style: TextStyle(fontSize: 12, color: Colors.white30),
+              )
+            ],
+          ),
+          Spacer(),
+        IconButton(onPressed: widget.link as void Function()?, icon: Icon(Icons.navigate_next_rounded))
+        ],
+      ),
     );
   }
 }
