c796efe518082f4f433ba39134c717ca8a1597eb
Youssef Lasheen <youssef@lasheen.dev>
2022-03-20 02:05:04 -0700

    Make sure the widget doesn't error when the document doesn't exist

--- lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
+++ lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
@@ -1,12 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import 'package:portfolio/screens/projects_screen/models/project.dart';
-import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/widgets/access_options.dart';
 
+import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/widgets/access_options.dart';
 import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/widgets/project_details_screen_top_landscape.dart';
 import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/widgets/project_details_screen_top_portrait.dart';
+
 import 'package:portfolio/services/api.dart';
 
 class ProjectDetailsScreen extends StatelessWidget {
@@ -21,50 +22,71 @@ class ProjectDetailsScreen extends StatelessWidget {
         MediaQuery.of(context).orientation == Orientation.landscape;
 
     return FutureBuilder<DocumentSnapshot>(
-        future: Future.delayed(
-            const Duration(
-              milliseconds: 350,
-            ), () {
+      future: Future.delayed(
+        const Duration(
+          milliseconds: 350,
+        ),
+        () {
           return Api('Data/projects_screen/projects').getDocumentById(id);
-        }),
-        builder: (context, snapshot) {
-          ProjectDescription projectDescription;
-          if (!snapshot.hasData) {
-            projectDescription = ProjectDescription(infoSnippet: {
-              'role': 'Loading..',
-              'client': 'Loading..',
-              'credits': 'Loading..',
-              'date': 'Loading..',
-              'longDescription': 'Loading..'
-            }, projectMetadata: ProjectMetadata(backgroundImageSource: null,shortDescription: '',title: '',tags: [],id: ''),accessOptions: [{'value':'','url':''}]);
-          } else {
-            projectDescription = ProjectDescription(
-                infoSnippet: snapshot.data!['infoSnippet'],
-                projectMetadata: ProjectMetadata(
-                  title: snapshot.data!['metadata']['title'],
-                  id: snapshot.data!['metadata']['id'],
-                  shortDescription: snapshot.data!['metadata']['shortDescription'],
-                  tags: snapshot.data!['metadata']['tags'],
-                  backgroundImageSource: snapshot.data!['metadata']['backgroundImageSource'],
-                ),
-                accessOptions: snapshot.data!['accessOptions']);
-          }
+        },
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              "Something went wrong",
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          );
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Center(
+            child: Text(
+              "404\nProject not found",
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          ProjectDescription projectDescription = ProjectDescription(
+            infoSnippet: data['infoSnippet'],
+            projectMetadata: ProjectMetadata(
+              title: data['metadata']['title'],
+              id: data['metadata']['id'],
+              shortDescription: data['metadata']['shortDescription'],
+              tags: data['metadata']['tags'],
+              backgroundImageSource: data['metadata']['backgroundImageSource'],
+            ),
+            accessOptions: data['accessOptions'],
+          );
           if (isLandscape) {
             return ProjectDetailsScreenTopLandscape(
-              isInversed: isInversed??true,
+              isInversed: isInversed ?? true,
               projectDescription: projectDescription,
-              
             );
           } else {
             return Stack(
               children: [
                 ProjectDetailsScreenTopPotrait(
-                  projectDescription: projectDescription ,
+                  projectDescription: projectDescription,
                 ),
-                AccessOptions(accessOptions: projectDescription.accessOptions,),
+                AccessOptions(
+                  accessOptions: projectDescription.accessOptions,
+                ),
               ],
             );
           }
-        });
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
