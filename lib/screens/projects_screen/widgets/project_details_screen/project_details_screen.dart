74820b95d3d0b6b98ae0d682950cff6fd1e1ebc4
Youssef Lasheen <youssef@lasheen.dev>
2022-03-20 04:26:49 -0700

    Add ProjectDetailsLoadingShimmerLandscape() to show a shimmer loading animation with the shimmer package

--- lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
+++ lib/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio/assets/constants.dart';
 
 import 'package:portfolio/screens/projects_screen/models/project.dart';
 
@@ -9,6 +10,7 @@ import 'package:portfolio/screens/projects_screen/widgets/project_details_screen
 import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/widgets/project_details_screen_top_portrait.dart';
 
 import 'package:portfolio/services/api.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ProjectDetailsScreen extends StatelessWidget {
   const ProjectDetailsScreen({this.isInversed, @PathParam() this.id});
@@ -82,11 +84,87 @@ class ProjectDetailsScreen extends StatelessWidget {
             );
           }
         }
-
-        return Center(
-          child: CircularProgressIndicator(),
-        );
+        return isLandscape
+            ? ProjectDetailsLoadingShimmerLandscape()
+            : Center(
+                child: CircularProgressIndicator(),
+              );
       },
+    );
+  }
+}
+
+class ProjectDetailsLoadingShimmerLandscape extends StatelessWidget {
+  const ProjectDetailsLoadingShimmerLandscape({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Shimmer(
+      gradient: shimmerGradient,
+      period: Duration(seconds: 1),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2,),
+                  Flexible(
+                    child: Container(
+                      height: 50,
+                      width: 200,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Flexible(
+                    child: Container(
+                      height: 50,
+                      width: 200,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Wrap(
+                    alignment: WrapAlignment.end,
+                    children: [
+                      for (var i = 0; i < 2; i++)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Chip(
+                              label: SizedBox(
+                            width: 50,
+                          )),
+                        )
+                    ],
+                  ),
+                  Spacer(
+                    flex: 3,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      height: screenHeight * 0.1,
+                      child: Container(color: Colors.white),
+                    ),
+                  ),
+                  Spacer()
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
