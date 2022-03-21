46881c94379746115d3f31d78c6c0cc4cc210d7f
Youssef Lasheen <youssef@lasheen.dev>
2022-03-21 18:13:34 +0200

    Add on hover animation to the frosted conatiner widget with foil and use riverpod to rebuild the backgound only for better performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/navigation/root.dart';
 import 'package:portfolio/routes/router.gr.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/shared_components/frostedglass_container.dart
+++ lib/screens/shared_components/frostedglass_container.dart
@@ -1,9 +1,18 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:foil/foil.dart';
 import 'package:portfolio/assets/constants.dart';
+import 'package:shimmer/shimmer.dart';
 
-class FrostedGlassContainer extends StatelessWidget {
+
+
+final hoveringStateProvider = StateProvider<bool>((ref) {
+  return false;
+});
+
+class FrostedGlassContainer extends ConsumerWidget {
   final Widget? child;
   final BorderRadius borderRadius;
   final Decoration decoration;
@@ -15,47 +24,67 @@ class FrostedGlassContainer extends StatelessWidget {
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ClipRRect(
       borderRadius: borderRadius,
-      child: Stack(
-        children: [
-          Positioned.fill(
-              child: Container(
-            decoration: decoration,
-            
-          )),
-          Positioned.fill(
-            child: BackdropFilter(
-              filter: new ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-              child: new Container(
-              )
+      child: MouseRegion(
+        onEnter: (_){
+          ref.read(hoveringStateProvider.state).state = true;
+        },
+        onExit: (_){
+          ref.read(hoveringStateProvider.state).state = false;
+        },
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Consumer(
+                builder: (_, WidgetRef ref, __){
+                  final value = ref.watch(hoveringStateProvider);
+                  return Foil(
+                    gradient: shimmerGradient,
+                    unwrappedGradient: mainGradient,
+                    useSensor: false,
+                    
+                    isUnwrapped: value,
+                    child: Container(
+                    decoration: decoration,
+                ),
+                  );
+                },
+              ),
             ),
-          ),
-          Positioned.fill(
-            child: Opacity(
-                  opacity: 0.1,
-                  child: Image.asset(
-                    "assets/textures/noise-300x300.png",
-                    fit: BoxFit.scaleDown,
-                    repeat: ImageRepeat.repeat,
-                  )),
-          ),
-          Container(
-              decoration: BoxDecoration(
-                  boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.25), blurRadius: 30, offset: Offset(2, 2))],
-                  borderRadius: borderRadius,
-                  border: Border.all(color: Colors.white.withOpacity(0.3), width: 0.3),
-                  gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
-                    Colors.white.withOpacity(0.1),
-                    Colors.white.withOpacity(0.08),
-                  ], stops: [
-                    0.0,
-                    1.0,
-                  ])),
-              child: child,
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: new ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: new Container(
+                )
+              ),
             ),
-        ],
+            Positioned.fill(
+              child: Opacity(
+                    opacity: 0.1,
+                    child: Image.asset(
+                      "assets/textures/noise-300x300.png",
+                      fit: BoxFit.scaleDown,
+                      repeat: ImageRepeat.repeat,
+                    )),
+            ),
+            Container(
+                decoration: BoxDecoration(
+                    boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.25), blurRadius: 30, offset: Offset(2, 2))],
+                    borderRadius: borderRadius,
+                    border: Border.all(color: Colors.white.withOpacity(0.3), width: 0.3),
+                    gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
+                      Colors.white.withOpacity(0.1),
+                      Colors.white.withOpacity(0.08),
+                    ], stops: [
+                      0.0,
+                      1.0,
+                    ])),
+                child: child,
+              ),
+          ],
+        ),
       ),
     );
   }
