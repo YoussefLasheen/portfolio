c1c1c864ce16fa09d7a882670ae03bcee21ab8a5
Youssef Lasheen <youssef@lasheen.dev>
2023-01-09 22:07:08 +0200

    Upgrade firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/routes/router.gr.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(
     child: MyApp(),
   ));
