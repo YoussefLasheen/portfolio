ca60d50e19b9bb5712a9312e59a683f747557bc9
Youssef Lasheen <youssef@lasheen.dev>
2023-01-13 21:18:00 +0200

    Remove unused riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:portfolio/navigation/root.dart';
 import 'package:portfolio/screens/about_screen/about_screen.dart';
-import 'package:portfolio/screens/contact_screen/contact_screen.dart';
-import 'package:portfolio/screens/projects_screen/projects_screen.dart';
-import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart';
+import 'package:portfolio/screens/contact_screen/contact_screen.dart' deferred as contact_screen;
+import 'package:portfolio/screens/projects_screen/projects_screen.dart' deferred as projects_screen;
+import 'package:portfolio/screens/projects_screen/widgets/project_details_screen/project_details_screen.dart' deferred as project_details;
 
 import 'firebase_options.dart';
 import 'screens/projects_screen/models/project.dart';
@@ -14,9 +13,7 @@ import 'screens/projects_screen/models/project.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(ProviderScope(
-    child: MyApp(),
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -48,7 +45,16 @@ class MyApp extends StatelessWidget {
           ) {
             return CustomTransitionPage(
               key: state.pageKey,
-              child: ProjectsScreen(queryTag: state.queryParams['tag']),
+              child: FutureBuilder(
+                future: projects_screen.loadLibrary(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return projects_screen.ProjectsScreen(queryTag: state.queryParams['tag']);
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
               transitionDuration: Duration.zero,
               transitionsBuilder: (_, __, ___, child) => child,
             );
@@ -62,9 +68,18 @@ class MyApp extends StatelessWidget {
               ) {
                 return CustomTransitionPage(
                   key: state.pageKey,
-                  child: ProjectDetailsScreen(
-                    id: state.params['id'],
-                    project: state.extra as Project?,
+                  child: FutureBuilder(
+                    future: project_details.loadLibrary(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return project_details.ProjectDetailsScreen(
+                          id: state.params['id'],
+                          project: state.extra as Project?,
+                        );
+                      } else {
+                        return SizedBox.shrink();
+                      }
+                    },
                   ),
                   transitionDuration: Duration.zero,
                   transitionsBuilder: (_, __, ___, child) => child,
@@ -81,7 +96,16 @@ class MyApp extends StatelessWidget {
           ) {
             return CustomTransitionPage(
               key: state.pageKey,
-              child: ContactScreen(),
+              child: FutureBuilder(
+                future: contact_screen.loadLibrary(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return contact_screen.ContactScreen();
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
               transitionDuration: Duration.zero,
               transitionsBuilder: (_, __, ___, child) => child,
             );
--- lib/screens/shared_components/frostedglass_container.dart
+++ lib/screens/shared_components/frostedglass_container.dart
@@ -1,16 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/assets/constants.dart';
 
-
-
-final hoveringStateProvider = StateProvider<bool>((ref) {
-  return false;
-});
-
-class FrostedGlassContainer extends ConsumerWidget {
+class FrostedGlassContainer extends StatelessWidget {
   final Widget? child;
   final BorderRadius borderRadius;
   final Decoration decoration;
@@ -22,60 +15,47 @@ class FrostedGlassContainer extends ConsumerWidget {
       : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context,) {
     return ClipRRect(
       borderRadius: borderRadius,
-      child: MouseRegion(
-        onEnter: (_){
-          ref.read(hoveringStateProvider.state).state = true;
-        },
-        onExit: (_){
-          ref.read(hoveringStateProvider.state).state = false;
-        },
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Consumer(
-                builder: (_, WidgetRef ref, __){
-                  final value = ref.watch(hoveringStateProvider);
-                  return Container(
-                    decoration: decoration,
-                );
-                },
-              ),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+                  decoration: decoration,
+              )
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: new ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: new Container(
+              )
             ),
-            Positioned.fill(
-              child: BackdropFilter(
-                filter: new ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                child: new Container(
-                )
-              ),
+          ),
+          Positioned.fill(
+            child: Opacity(
+                  opacity: 0.1,
+                  child: Image.asset(
+                    "assets/textures/noise-300x300.png",
+                    fit: BoxFit.scaleDown,
+                    repeat: ImageRepeat.repeat,
+                  )),
+          ),
+          Container(
+              decoration: BoxDecoration(
+                  boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.25), blurRadius: 30, offset: Offset(2, 2))],
+                  borderRadius: borderRadius,
+                  border: Border.all(color: Colors.white.withOpacity(0.3), width: 0.3),
+                  gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
+                    Colors.white.withOpacity(0.1),
+                    Colors.white.withOpacity(0.08),
+                  ], stops: [
+                    0.0,
+                    1.0,
+                  ])),
+              child: child,
             ),
-            Positioned.fill(
-              child: Opacity(
-                    opacity: 0.1,
-                    child: Image.asset(
-                      "assets/textures/noise-300x300.png",
-                      fit: BoxFit.scaleDown,
-                      repeat: ImageRepeat.repeat,
-                    )),
-            ),
-            Container(
-                decoration: BoxDecoration(
-                    boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.25), blurRadius: 30, offset: Offset(2, 2))],
-                    borderRadius: borderRadius,
-                    border: Border.all(color: Colors.white.withOpacity(0.3), width: 0.3),
-                    gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
-                      Colors.white.withOpacity(0.1),
-                      Colors.white.withOpacity(0.08),
-                    ], stops: [
-                      0.0,
-                      1.0,
-                    ])),
-                child: child,
-              ),
-          ],
-        ),
+        ],
       ),
     );
   }
