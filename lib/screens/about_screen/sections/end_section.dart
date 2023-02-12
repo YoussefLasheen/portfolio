5e0592795898623dda5ac6fd14f7e25c67a0b5fd
Youssef Lasheen <youssef@lasheen.dev>
2023-02-12 23:33:51 +0200

    Show the phone number as a link

--- lib/screens/about_screen/sections/end_section.dart
+++ lib/screens/about_screen/sections/end_section.dart
@@ -1,7 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:portfolio/screens/shared_components/frostedglass_container.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EndSection extends StatelessWidget {
   @override
@@ -9,19 +11,23 @@ class EndSection extends StatelessWidget {
     bool isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
     return Padding(
-      padding: isLandscape? EdgeInsets.only(right: 100, left: 50, bottom: 100): EdgeInsets.zero,
+      padding: isLandscape
+          ? EdgeInsets.only(right: 100, left: 50, bottom: 100)
+          : EdgeInsets.zero,
       child: ConstrainedBox(
-        constraints: BoxConstraints.expand(height: isLandscape? 500: 800),
+        constraints: BoxConstraints.expand(height: isLandscape ? 500 : 800),
         child: FrostedGlassContainer(
-          borderRadius: isLandscape?BorderRadius.circular(20.0):BorderRadius.circular(0.0),
+          borderRadius: isLandscape
+              ? BorderRadius.circular(20.0)
+              : BorderRadius.circular(0.0),
           child: Center(
             child: FractionallySizedBox(
-              heightFactor: 8/10,
-              widthFactor: 9/10,
+              heightFactor: 8 / 10,
+              widthFactor: 9 / 10,
               child: Flex(
-                direction: isLandscape? Axis.horizontal:Axis.vertical,
+                direction: isLandscape ? Axis.horizontal : Axis.vertical,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                 children: [
+                children: [
                   Expanded(
                     flex: 3,
                     child: Column(
@@ -33,12 +39,39 @@ class EndSection extends StatelessWidget {
                           maxLines: 2,
                           style: TextStyle(fontSize: 70),
                         ),
-                        SizedBox(height: 10,),
+                        SizedBox(
+                          height: 10,
+                        ),
                         Flexible(
-                          child: AutoSizeText(
-                            'If you would like to discuss your project with us and receive a free consultation, please contact us via our website or call us at +201012845227. \nWe look forward to hearing from you!',
+                          child: AutoSizeText.rich(
+                            TextSpan(
+                              style: TextStyle(fontSize: 25),
+                              children: [
+                                TextSpan(
+                                  text:
+                                      'If you would like to discuss your project with us and receive a free consultation, please contact us via our website or call us at ',
+                                ),
+                                TextSpan(
+                                  text: '+201012845227',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () => launchUrl(Uri(
+                                        scheme: 'tel', path: '+201012845227')),
+                                ),
+                                TextSpan(
+                                  text:
+                                      '\nWe look forward to hearing from you!',
+                                ),
+                              ],
+                            ),
                             textAlign: TextAlign.justify,
-                            style: TextStyle(color: Colors.white54, fontSize: 25, height: 1.5),
+                            style: TextStyle(
+                                color: Colors.white54,
+                                fontSize: 25,
+                                height: 1.5),
                           ),
                         )
                       ],
@@ -53,7 +86,7 @@ class EndSection extends StatelessWidget {
                           borderRadius: new BorderRadius.circular(5.0),
                         ),
                       ),
-                      onPressed: ()=> context.go('/contact'),
+                      onPressed: () => context.go('/contact'),
                       child: Padding(
                         padding: const EdgeInsets.all(18.0),
                         child: Text(
@@ -68,10 +101,10 @@ class EndSection extends StatelessWidget {
                   )
                 ],
               ),
-                  ),
-          ),
-              ),
             ),
+          ),
+        ),
+      ),
     );
   }
 }
